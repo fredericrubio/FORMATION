ae5662234e24c4ff735e4e469374bfd6e0a6df53
Frederic Rubio <frederic.rubio@magellium.fr>
2016-09-21 15:34:05 +0200

    Ajout expressions sur commande Copier-coller
    Change-Id: I9238f999b3aa3b56eb30c5f6455f511e9064ef03
    Signed-off-by: Frederic Rubio <frederic.rubio@magellium.fr>

--- com.magellium.rental.ui/src/com/magellium/rental/ui/handler/HelloWorldHandler.java
+++ com.magellium.rental.ui/src/com/magellium/rental/ui/handler/HelloWorldHandler.java
@@ -1,10 +1,14 @@
 package com.magellium.rental.ui.handler;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -17,6 +21,7 @@ public class HelloWorldHandler extends AbstractHandler implements IHandler {
 		
 		window = HandlerUtil.getActiveWorkbenchWindow(event);
 		MessageDialog.openInformation(window.getShell(), "PluginID", "Hello World!");
+
 
 		return null;
 	}
