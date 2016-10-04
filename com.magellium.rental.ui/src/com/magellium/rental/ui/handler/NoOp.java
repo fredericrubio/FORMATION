6a81ae55331544d5f55372b5e246e5a34fa55bda
fredericrubio <fredericrubio@192.168.0.16>
2016-10-04 22:10:38 +0200

    Assignation directe de l'état
    Change-Id: I0a6a1cd018a7968045c9e13a64bbe4b53a99ea6c
    Signed-off-by: fredericrubio <fredericrubio@192.168.0.16>

--- com.magellium.rental.ui/src/com/magellium/rental/ui/handler/NoOp.java
+++ com.magellium.rental.ui/src/com/magellium/rental/ui/handler/NoOp.java
@@ -7,18 +7,32 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.handlers.RegistryToggleState;
+import org.eclipse.core.commands.State;
 
 public class NoOp extends AbstractHandler{
 
+	static boolean lStatus = true;
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
 		
 		Command command = service.getCommand("com.magellium.rental.ui.rental.helloworldcommand");
+//		State state = command.getState(RegistryToggleState.STATE_ID);
+		lStatus = !lStatus;
+//		state.setValue(Boolean.valueOf(lStatus));
+		State lState2 = command.getState("org.eclipse.ui.commands.toggleState");
+		lState2.setValue(Boolean.valueOf(lStatus));
 				
-//		Command command = event.getCommand();
-	     boolean oldValue = HandlerUtil.toggleCommandState(command);
+/*		Command command2 = event.getCommand();
+		State lState2 = command2.getState("org.eclipse.ui.commands.toggleState");
+		
+		lState2.setValue(lStatus);
+		*/
+		
+//		boolean oldValue = HandlerUtil.toggleCommandState(command);
 	     
 		return null;
 	}
